f1c16d4a63ee8c71c001433820daf453bcfe8772
gkiplangat <gkiplangat>
2025-01-23 19:53:29 +0300

    global variables

--- variables.py
+++ variables.py
@@ -1,7 +1,16 @@
 def name():
     # Declare the global variables
     global name
+    global fruits
     name = input("Enter your name: ")
+    fruits = ['Apple', 'Banana', 'mango',['Carrots', 'Tomatoes']]
 
 name()
 print("Dear " +name+", Welcome to Riara University")
+
+for i in name:
+    print(i)
+
+for fruit in fruits:
+    print(fruit)
+print(fruits[3][1])
