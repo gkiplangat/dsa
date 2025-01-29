150a0801d13613403e31a78b6f2f3d40dbfeefeb
gkiplangat <gkiplangat>
2025-01-29 21:50:15 +0300

    even numbers

--- lists/easy/list014_odd_numbers.py
+++ lists/easy/list014_odd_numbers.py
@@ -1,6 +1,11 @@
+# 14. ** Get all odd numbers from a list.**
+#- Write a Python function that returns all the odd numbers in a list.
 def odd_numbers(lst):
     # Your implementation here
-    pass  # Replace with your code
-
+    odd_numbers = []
+    for i in lst:
+        if i % 2 == 1:
+            odd_numbers.append(i)
+    return odd_numbers
 
 print(odd_numbers([1, 2, 3, 4, 5, 6]))  # Expected Output: [1, 3, 5]
