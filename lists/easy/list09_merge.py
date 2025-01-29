885406cad3c8787bb4b8a0551726ded67530bb7e
gkiplangat <gkiplangat>
2025-01-29 19:14:20 +0300

    find min

--- lists/easy/list09_merge.py
+++ lists/easy/list09_merge.py
@@ -1,6 +1,9 @@
+# 9. ** Merge two lists.**
+# - Write a Python function that merges two lists into one.
 def merge_lists(lst1, lst2):
     # Your implementation here
-    pass  # Replace with your code
+    lst1.extend(lst2)
+    return lst1
 
 
 print(merge_lists([1, 2, 3], [4, 5, 6]))  # Expected Output: [1, 2, 3, 4, 5, 6]
