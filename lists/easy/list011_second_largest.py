000281188472f5cfea27ad62e95fa66a9647a4d3
gkiplangat <gkiplangat>
2025-01-29 21:21:02 +0300

    return the second largest number

--- lists/easy/list011_second_largest.py
+++ lists/easy/list011_second_largest.py
@@ -3,13 +3,8 @@
 #- Write a Python function to return the second-largest number in a list.
 def second_largest(lst):
     # Your implementation here
-    n = len(lst)-1
-    sorted = 0
-    for i in range(n):
-        if lst[i] > lst[i+1]:
-            
-
-    return sorted
+    sorted = lst.sort()
+    return lst[-2]
 
 
 print(second_largest([1, 2, 3, 4, 5]))  # Expected Output: 4
--- lists/easy/list013_even_numbers.py
+++ lists/easy/list013_even_numbers.py
@@ -1,6 +1,12 @@
+#13. ** Get all even numbers from a list.**
+#- Write a Python function that returns all the even numbers in a list.
+
 def even_numbers(lst):
     # Your implementation here
-    pass  # Replace with your code
+    even_numbers = 0
+    
+
+
 
 
 print(even_numbers([1, 2, 3, 4, 5, 6]))  # Expected Output: [2, 4, 6]
