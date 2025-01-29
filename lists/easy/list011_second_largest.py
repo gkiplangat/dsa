3900d227837b15a2fc4924a091321ba70a1129eb
gkiplangat <gkiplangat>
2025-01-29 20:40:02 +0300

    return the last element in a list

--- lists/easy/list011_second_largest.py
+++ lists/easy/list011_second_largest.py
@@ -1,6 +1,15 @@
+
+#11. ** Get the second-largest element in a list.**
+#- Write a Python function to return the second-largest number in a list.
 def second_largest(lst):
     # Your implementation here
-    pass  # Replace with your code
+    n = len(lst)-1
+    sorted = 0
+    for i in range(n):
+        if lst[i] > lst[i+1]:
+            
+
+    return sorted
 
 
 print(second_largest([1, 2, 3, 4, 5]))  # Expected Output: 4
--- lists/easy/list012_last_element.py
+++ lists/easy/list012_last_element.py
@@ -1,6 +1,9 @@
+#12. ** Access the last element of a list.**
+#- Write a Python function to return the last element of a list.
+
 def last_element(lst):
     # Your implementation here
-    pass  # Replace with your code
+    return lst[-1]
 
 
-print(last_element([1, 2, 3, 4, 5]))  # Expected Output: 5
+print(last_element([1, 2, 3, 4, 5, 8]))  # Expected Output: 5
