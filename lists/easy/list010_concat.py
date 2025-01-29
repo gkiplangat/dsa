70b4593e4b7232808235cc6ab03d2c751b6c35ab
gkiplangat <gkiplangat>
2025-01-29 19:17:31 +0300

    find min

--- lists/easy/list010_concat.py
+++ lists/easy/list010_concat.py
@@ -1,6 +1,10 @@
+# 10. ** Concatenate two lists.**
+# - Write a Python function to concatenate two lists.
+
 def concatenate_lists(lst1, lst2):
     # Your implementation here
-    pass  # Replace with your code
+    lst1 = lst1 +lst2
+    return lst1
 
 
 # Expected Output: [1, 2, 3, 4, 5, 6]
