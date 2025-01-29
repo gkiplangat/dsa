eca54939af79e0c31f1cd0aa8112c73aad1cfe91
gkiplangat <gkiplangat>
2025-01-29 21:43:05 +0300

    even numbers

--- lists/easy/list015_sort_asc.py
+++ lists/easy/list015_sort_asc.py
@@ -1,6 +1,10 @@
+#15. ** Sort a list in ascending order.**
+#- Write a Python function to sort a list of numbers in ascending order.
+
 def sort_list(lst):
     # Your implementation here
-    pass  # Replace with your code
+    lst.sort()
+    return lst
 
 
 print(sort_list([5, 2, 3, 1, 4]))  # Expected Output: [1, 2, 3, 4, 5]
